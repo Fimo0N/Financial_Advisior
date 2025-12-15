07282f49c0b3fdb89f605b5991cac532fc722e83
Fodor Tamas <fodor.tamas2005@gmail.com>
2025-12-15 15:19:31 +0100

    Replace remaining Alpha Vantage logic with Twelve Data logic

--- src/config/apiKeys.js
+++ src/config/apiKeys.js
@@ -1,7 +1,7 @@
 // This file stores your API keys.
 // Remember to keep this file secure and out of public version control in a real project.
 
-//export const alphaVantageApiKey = '4FOEYF3LTE8IGYRO';
+//export const alphaVantageApiKey = '';
 export const newsApiKey = '';
 export const geminiApiKey = '';
 export const twelveDataApiKey = '';
